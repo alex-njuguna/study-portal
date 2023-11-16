10b390357f235f20081cc036d25eeba3bad79722
alex-njuguna <njugunakinuthia013@gmail.com>
2023-11-17 01:12:49 +0300

    final touches on dashboard

--- portal/dashboard/views.py
+++ portal/dashboard/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 
 def home(request):
 
-    return render(request, "dashboard/home.html")
+    return render(request, "dashboard/home.html", {"title": "dashboard"})
