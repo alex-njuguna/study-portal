60092c29b0e8ce5e9ee6135baa8ec0b2dbed3aa9
alex-njuguna <njugunakinuthia013@gmail.com>
2023-11-18 02:28:15 +0300

    display all homeworks functionality

---
+++ portal/homework/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+
+from .models import Homework
+
+
+def home(request):
+    """
+    get all homeworks for a specific user
+    display a form to add a homework
+    """
+    homeworks = Homework.objects.filter(user=request.user)
+
+    return render(request, "homework/home.html", {
+        "title": "homework",
+        "homeworks": homeworks
+    })
+
+# def update_homework(request, id):
+#     """
+#     update homework to either complete or not
+#     """
+#     homework = Homework.objects.get(user=request.user, id=id)
+
+#     if homework.is_finished == True:
+#         homework.is_finished = False
+#         messages.info(request, f"{homework.title} marked as NOT finished")
+#     else:
+#         homework.is_finished = True
+#         messages.success(request, f"{homework.title} marked as FINISHED")
+    
+#     return redirect("homework:home")
+
+
+
+
