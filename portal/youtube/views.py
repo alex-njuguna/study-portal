53343bba319bd5d89c1382826e2c8c91d9ab60d6
alex-njuguna <njugunakinuthia013@gmail.com>
2023-11-19 01:27:43 +0300

    search, display and redirect to youtube

--- portal/youtube/views.py
+++ portal/youtube/views.py
@@ -9,7 +9,42 @@ def home(request):
     display a search form
     display all videos searched
     """
-    form = SearchYoutubeForm()
+    if request.method == "POST":
+        form = SearchYoutubeForm(request.POST)
+
+        if form.is_valid():
+            text = form.cleaned_data.get("text")
+            videos = VideosSearch(text, limit=10)
+
+            result_list = []
+            for video in videos.result()["result"]:
+                result_dict = {
+                    "input": text,
+                    "title": video["title"],
+                    "duration": video["duration"],
+                    "thumbnail": video["thumbnails"][0]["url"],
+                    "channel": video["channel"]["name"],
+                    "link": video["link"],
+                    "views": video["viewCount"]["short"],
+                    "published": video["publishedTime"]
+                }
+
+                desc = ""
+                if video["descriptionSnippet"]:
+                    for snip in video["descriptionSnippet"]:
+                        desc += snip["text"]
+
+                result_dict["description"] = desc
+
+                result_list.append(result_dict)
+
+            return render(request, "youtube/home.html", {
+                "results": result_list,
+                "title": "youtube",
+                "form": form
+            })
+    else:
+        form = SearchYoutubeForm()
 
     return render(request, "youtube/home.html", {
         "title": "youtube",
