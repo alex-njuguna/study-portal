fc8c864a76a04e6909983f1fadf3155889c6d1b1
alex-njuguna <njugunakinuthia013@gmail.com>
2023-11-20 01:39:05 +0300

    update acctivity functionality

--- portal/activity/views.py
+++ portal/activity/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
 
 from .models import Activity
 from .forms import ActivityForm
@@ -15,3 +16,21 @@ def home(request):
         "title": "activities",
         "activities": activities
     })
+
+
+def update_activity(request, id):
+    """
+    update the status of the activity from either
+    is_finished or not and vice-versa
+    """
+    activity = Activity.objects.get(user=request.user, id=id)
+
+    if activity.is_finished == False:
+        activity.is_finished = True
+        activity.save()
+        messages.info(request, f"Status for activity {activity.title.upper()} updated")
+    else:
+        activity.is_finished = False
+        activity.save()
+        messages.info(request, f"Status for activity {activity.title.upper()} updated")
+    return redirect("activity:home")
