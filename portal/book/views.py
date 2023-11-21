02082059a3c7cf82dc33f2674b436d04cc47f037
alex-njuguna <njugunakinuthia013@gmail.com>
2023-11-21 03:14:06 +0300

    book search, display and functionality

--- portal/book/views.py
+++ portal/book/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 import requests
+import json
 
 from .forms import SearchBookForm
 
@@ -10,8 +11,39 @@ def home(request):
     display 10 books searched
     api - https://www.googleapis.com/books/v1/volumes?q={search terms}
     """
+    if request.method == "POST":
+        form = SearchBookForm(request.POST)
 
-    form = SearchBookForm()
+        if form.is_valid():
+            text = form.cleaned_data.get("text")
+
+            url = f"https://www.googleapis.com/books/v1/volumes?q={text}"
+            response = requests.get(url)
+            data = response.json()
+
+            books = []
+            for item in data["items"]:
+                book = {}
+                volume_info = item['volumeInfo']
+                book['title'] = volume_info.get('title')
+                book['subtitle'] = volume_info.get('subtitle')
+                book['description'] = volume_info.get('description')
+                book['count'] = volume_info.get('pageCount')
+                book['categories'] = volume_info.get('categories')
+                book['rating'] = volume_info.get('averageRating')
+                book['thumbnail'] = volume_info.get('imageLinks', {}).get('thumbnail')
+                book['preview'] = volume_info.get('previewLink')
+
+                books.append(book)
+
+            return render(request, "book/home.html", {
+                "title": "books",
+                "books": books,
+                "form": form
+            })
+
+    else:
+        form = SearchBookForm()
 
     return render(request, "book/home.html", {
         "title": "books",
