227838cca397cdb422e69ddfd7e7a7aab81c5f27
alex-njuguna <njugunakinuthia013@gmail.com>
2023-11-23 02:13:10 +0300

    search word using dictionary api functionality

--- portal/dictionary/forms.py
+++ portal/dictionary/forms.py
@@ -2,7 +2,7 @@ from django import forms
 
 """form with text field to collect a search term"""
 class SearchDictionaryForm(forms.Form):
-    text = forms.TextInput(label="Search word:", 
+    text = forms.CharField(max_length=50, label="Search word:", 
                            widget=forms.TextInput(attrs={
                                "class": "form-control",
                                "placeholder": "family"
--- portal/dictionary/views.py
+++ portal/dictionary/views.py
@@ -1,3 +1,64 @@
+import requests
+
 from django.shortcuts import render
 
-# Create your views here.
+from .forms import SearchDictionaryForm
+
+
+def home(request):
+    """
+    display search form
+    display search results
+    api - "https://api.dictionaryapi.dev/api/v2/entries/en_US/"+text
+    """
+    if request.method == "POST":
+        form = SearchDictionaryForm(request.POST)
+
+        if form.is_valid():
+            text = form.cleaned_data.get("text")
+            
+            url = f"https://api.dictionaryapi.dev/api/v2/entries/en_US/{text}"
+            r = requests.get(url)
+            response = r.json()
+
+            try:
+                phonetic = response[0]["phonetics"][0]
+                meaning = response[0]["meanings"][0]["definitions"][0]
+
+                phonetics = phonetic["text"]
+                audio = phonetic["audio"]
+                definition = meaning["definition"]
+                example = meaning["example"]
+                synonyms = meaning["synonyms"]
+
+                context = {
+                    "title": "dictionary",
+                    "form": form,
+                    "input": text,
+                    "phonetics": phonetics,
+                    "audio": audio,
+                    "definition": definition,
+                    "example": example,
+                    "synonyms": synonyms
+                }
+
+
+            except:
+                context = {
+                    "form": form,
+                    "title": "dictionary",
+                    "input": ""
+                }
+            
+            return render(request, "dictionary/home.html", context)
+            
+    else:
+        form = SearchDictionaryForm()
+        context = {
+            "form": form,
+            "title": "dictionary"
+        }
+    
+    return render(request, "dictionary/home.html", context)
+    
+
