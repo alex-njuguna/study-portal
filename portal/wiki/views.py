55d14a1da57d088d2c93df0ed580651380da07c7
alex-njuguna <njugunakinuthia013@gmail.com>
2023-11-24 02:06:54 +0300

    search form functionality and templating

--- portal/wiki/views.py
+++ portal/wiki/views.py
@@ -1,3 +1,17 @@
+import wikipedia
 from django.shortcuts import render
 
-# Create your views here.
+from .forms import WikiSearchForm
+
+
+def home(request):
+    """
+    collect search parameters
+    display article from wiki
+    """
+    form = WikiSearchForm()
+
+    return render(request, "wiki/home.html", {
+        "title": "wikipedia",
+        "form": form
+    })
