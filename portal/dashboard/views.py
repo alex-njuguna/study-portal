4e853190d9c29ed603f8a23017dfcee82dc7c44f
alex-njuguna <njugunakinuthia013@gmail.com>
2023-11-25 02:01:12 +0300

    return all undone activities

--- portal/dashboard/views.py
+++ portal/dashboard/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
 
+from activity.models import Activity
+from homework.models import Homework
+
 
 def home(request):
 
     return render(request, "dashboard/home.html", {"title": "dashboard"})
+
+
+def profile(request):
+    """
+    return undone activities
+    return undone homeworks
+    """
+    activities = Activity.objects.filter(user=request.user, is_finished=False)
+
+    return render(request, "dashboard/profile.html", {
+        "acrivities": activities
+    })
