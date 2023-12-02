1ef3b4e4c813cbd3bd7f3faffec2b71ee178b31b
alex-njuguna <njugunakinuthia013@gmail.com>
2023-12-03 00:11:18 +0300

    working google signup

--- portal/dashboard/urls.py
+++ portal/dashboard/urls.py
@@ -9,6 +9,5 @@ urlpatterns = [
     path("", views.home, name="home"),
     path("profile/", views.profile, name="profile"),
     path("signout/", views.signout, name="signout")
-
 ]
 
--- portal/portal/settings.py
+++ portal/portal/settings.py
@@ -17,7 +17,7 @@ ALLOWED_HOSTS = []
 
 
 # Application definition
-SITE_ID = 1
+SITE_ID = 2
 
 INSTALLED_APPS = [
     'django.contrib.admin',
